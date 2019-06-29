4aa86332c5d1553a4b90242a89e0f9ec7469e553
Mai Ngo <mtngo@smith.edu>
2019-06-29 11:14:22 -0400

    add jewels and stones

---
+++ week2/771.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        jewels = set(J)
+        
+        count = 0
+        for c in S:
+            if c in jewels:
+                count += 1
+        return count
