50ebf0f3001dad6849524db7e53c95d4ee10544b
Mai Ngo <mtngo@smith.edu>
2019-06-30 20:05:05 -0400

    modify subdomain count

--- week2/811.py
+++ week2/811.py
@@ -20,4 +20,26 @@ class Solution(object):
         return [str(counter[sd]) + " " + sd for sd in counter]
         
         
+"""
+Solution with defaultdict (better overall performance)
+
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
         
+        
+        counter = collections.defaultdict(int)
+        
+        for cp in cpdomains:
+            split = cp.split()
+            count = int(split[0])
+            
+            address = split[1]
+            counter[address] += count
+            for i in range(len(address)):
+                if address[i] == ".":
+                    counter[address[i+1:]] += count
+        
+        return [str(counter[sd]) + " " + sd for sd in counter]
+        
+        
+"""        
