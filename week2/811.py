22fda73aff0fb873108a564977e93eef3cff8bba
Mai Ngo <mtngo@smith.edu>
2019-06-30 20:00:17 -0400

    add subdomain visit count

---
+++ week2/811.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        
+        counter = collections.Counter()
+        
+        for cp in cpdomains:
+            split = cp.split()
+            count = int(split[0])
+            
+            address = split[1]
+            counter.update({address:count})
+            for i in range(len(address)):
+                if address[i] == ".":
+                    counter.update({address[i+1:]:count})
+        
+        return [str(counter[sd]) + " " + sd for sd in counter]
+        
+        
+        
